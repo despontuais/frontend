42818f968dedd181a5b3441b4e2c5cd1ac568fbd
Pablo <pablorsantana@hotmail.com>
2024-06-04 03:40:38 -0300

    testing register route

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import register from './Register.module.css';
+"use client"
 
-const Register = () => {
+import React from 'react'
+import registerStyle from './Register.module.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+const RegisterScreen = () => {
+  //getting user input
+    const [usernameInput, setUsernameInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+    const [confirmPasswordInput, setconfirmPasswordInput] = useState('');
+    const [emailInput, setEmailInput] = useState('');
+    const [birthdateInput, setBirthdateInput] = useState('');
+
+    const handleRegister = () => {
+        register();
+    }
+  
+    
+    //testing login route
+    const register = async () => {
+      try{
+          const res = await axios.post('http://localhost:3000/api/auth/register', {
+              headers: {
+                  'Content-type': 'application/json',
+                  'Accept' : '*/*'
+              },
+                  email: emailInput,
+                  password: passwordInput,
+                  name: usernameInput,
+                  birthDate: birthdateInput
+          });
+         console.log(res.data);
+      }catch(err){
+          console.log(err);
+      }
+  }
+
+  
   return (
-    <div className={register.backgroundRegister}>
+    <div className={registerStyle.backgroundRegister}>
       <title>Cronolog</title>
 
       <h1>Área de Registro</h1>
@@ -13,28 +49,28 @@ const Register = () => {
       <form action="/api/register" method="POST">
         {/* parte das informações do usuário */}
 
-        <div className={register.userArea}>
+        <div className={registerStyle.userArea}>
           {/* usuario */}
-          <label htmlFor="User" className={register.label}>Usuário</label>
-          <input type="text" placeholder='Nome de Usuário' className={register.infoArea} name="user" id="User" />
+          <label htmlFor="User" className={registerStyle.label}>Usuário</label>
+          <input type="text" placeholder='Nome de Usuário' className={registerStyle.infoArea} name="user" id="User" value={usernameInput} onChange={e => setUsernameInput(e.target.value)} />
 
 
           {/* senha */}
-          <label htmlFor="Password" className={register.label}>Senha</label>
-          <input type="password" placeholder='Senha' className={register.infoArea} name="password" id="Password" />
+          <label htmlFor="Password" className={registerStyle.label}>Senha</label>
+          <input type="password" placeholder='Senha' className={registerStyle.infoArea} name="password" id="Password" value={passwordInput} onChange={e => setPasswordInput(e.target.value)} />
 
-          <label htmlFor="PasswordConfirmation" className={register.label}>Confirme a senha</label>
-          <input type="password" placeholder='Confirme a Senha' className={register.infoArea} name="PasswordConfirmation" id="PasswordConfirmation" />
+          <label htmlFor="PasswordConfirmation" className={registerStyle.label}>Confirme a senha</label>
+          <input type="password" placeholder='Confirme a Senha' className={registerStyle.infoArea} name="PasswordConfirmation" id="PasswordConfirmation" value={confirmPasswordInput} onChange={e => setconfirmPasswordInput(e.target.value)} />
           
 
           {/* email */}
-          <label htmlFor="email" className={register.label}>Email</label>
-          <input type="email" placeholder='Email' className={register.infoArea} name="email" id='email' required />
+          <label htmlFor="email" className={registerStyle.label}>Email</label>
+          <input type="email" placeholder='Email' className={registerStyle.infoArea} name="email" id='email' required value={emailInput} onChange={e => setEmailInput(e.target.value)} />
 
 
           {/* data de nasc */}
-          <label htmlFor="birthDate" className={register.label}>Data de nascimento</label>
-          <input type="date" placeholder='Data de Nascimento' className={register.infoArea} name="birthDate" id="birthDate" />
+          <label htmlFor="birthDate" className={registerStyle.label}>Data de nascimento</label>
+          <input type="date" placeholder='Data de Nascimento' className={registerStyle.infoArea} name="birthDate" id="birthDate" value={birthdateInput} onChange={e => setBirthdateInput(e.target.value)}/>
 
 
         </div>
@@ -42,7 +78,7 @@ const Register = () => {
         {/* parte dos botões */}
         <div>
           <input type="checkbox" name="terms" id="terms" required/> Declaro que li e aceito os termos e condições
-          <input className={register.btn} type="button" value="Registrar"/>
+          <input className={registerStyle.btn} type="button" value="Registrar" onClick={handleRegister}/>
         </div>
       
       </form>
@@ -50,4 +86,4 @@ const Register = () => {
   )
 }
 
-export default Register
+export default RegisterScreen
