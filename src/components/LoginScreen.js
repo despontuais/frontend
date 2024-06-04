ad17eeb48afc12dda2b38f5b7d802a4eaa0f89a5
Pablo <pablorsantana@hotmail.com>
2024-06-04 01:47:18 -0300

    creating ping function to test api adding logo image testing login route changing 'login' to 'loginStyle' so it does not conflict with the function 'login'

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,37 +1,86 @@
+"use client"
 //style
-import login from './LoginScreen.module.css';
+import loginStyle from './LoginScreen.module.css';
 
 //logo
 import cronologo from "../../public/Cronologo.png"
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const LoginScreen = () => {
 
+    const [emailInput, setEmailInput] = useState('');
+
+    const [passwordInput, setPasswordInput] = useState('');
+ 
+    const handleButtonClick = () => {
+        login();
+    }
+
+    const ping = async () => {
+        try{
+            const res = await fetch('http://localhost:3000/api/ping');
+            console.log(await res.json());
+        }catch(err){
+            console.log(err)
+        }
+        
+    }
+ 
+    useEffect(() => {
+        ping();
+    }, [])
+
+    const login = async () => {
+        try{
+            const res = await fetch('http://localhost:3000/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: emailInput,
+                    password: passwordInput
+                })
+            });
+            const json = await res.json();
+            console.log(json);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
     return (
-        <div className={login.backgroundLogin}>
+        <div className={loginStyle.backgroundLogin}>
             <title>Cronolog</title>
 
             <h1>CronoLogin</h1>
 
             {/* fazer a imagem aparecer aqui */}
-            <img src={cronologo} alt="nao ta funcionando imagem"/>
-            
+            <Image 
+                src={cronologo}
+                width={500}
+                height={500}
+                alt="Cronolog Logo"
+                />            
             <form action="">
                 {/* parte das informações do usuário */}
 
-                <div className={login.userArea}>
-                    <label htmlFor="User" className={login.label}>Usuário</label>
-                    <input type="text" placeholder='Usuário' className={login.infoArea} name="user" id="User" />
+                <div className={loginStyle.userArea}>
+                    <label htmlFor="User" className={loginStyle.label}>Usuário</label>
+                    <input type="text" placeholder='Usuário' className={loginStyle.infoArea} name="user" id="User" value={emailInput} onChange={e => setEmailInput(e.target.value)}/>
 
-                    <label htmlFor="Password" className={login.label}>Senha</label>
-                    <input type="password" placeholder='Senha' className={login.infoArea} name="password" id="Password" />
+                    <label htmlFor="Password" className={loginStyle.label}>Senha</label>
+                    <input type="password" placeholder='Senha' className={loginStyle.infoArea} name="password" id="Password" value={passwordInput} onChange={e => setPasswordInput(e.target.value)} />
                 </div>
 
                 {/* parte dos botões */}
-                <div className={login.logArea}>
-                    <input className={login.btn} type="button" value="Login" />
-                    <input className={login.btn} type="button" value="Registrar" />
+                <div className={loginStyle.logArea}>
+                    <input className={loginStyle.btn} type="button" value="Login" onClick={handleButtonClick} />
+                    <input className={loginStyle.btn} type="button" value="Registrar" />
                 </div>
-                <a href="/" className={login.forgetPassword}>Esqueci minha senha</a>
+                <a href="/" className={loginStyle.forgetPassword}>Esqueci minha senha</a>
             </form>
         </div>
     )
