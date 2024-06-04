9c8a1260f6f993104c79be673af799f91aa7059b
Pablo <pablorsantana@hotmail.com>
2024-06-04 03:38:59 -0300

    assuring LoginScreen.js is the same in Login and Register branch

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,37 +1,91 @@
+"use client"
 //style
-import login from './LoginScreen.module.css';
+import loginStyle from './LoginScreen.module.css';
+
+//request
+import axios from 'axios';
 
 //logo
 import cronologo from "../../public/Cronologo.png"
+import Image from 'next/image';
+
+//effect and state
+import { useEffect, useState } from 'react';
 
 const LoginScreen = () => {
+    
+    //testing ping route
+    const ping = async () => {
+        try{
+            const res = await axios.get('http://localhost:3000/api/ping');
+            console.log(res.data);
+        }catch(err){
+            console.log(err);
+        }
+    }
+ 
+    //ping the backend as soon as the page loads
+    useEffect(() => {
+        ping();
+    }, [])
+
+    //getting user input
+    const [emailInput, setEmailInput] = useState('');
+
+    const [passwordInput, setPasswordInput] = useState('');
+ 
+    const handleLogin = () => {
+        login();
+    }
+
+    //testing login route
+    const login = async () => {
+        try{
+            const res = await axios.post('http://localhost:3000/api/auth/login', {
+                headers: {
+                    'Content-type': 'application/json',
+                    'Accept' : '*/*'
+                },
+                    email: emailInput,
+                    password: passwordInput
+            });
+           console.log(res.data);
+        }catch(err){
+            //console.log(err);
+        }
+    }
+
 
     return (
-        <div className={login.backgroundLogin}>
+        <div className={loginStyle.backgroundLogin}>
             <title>Cronolog</title>
 
             <h1>CronoLogin</h1>
 
             {/* fazer a imagem aparecer aqui */}
-            <img src={cronologo} alt="nao ta funcionando imagem"/>
-            
+            <Image 
+                src={cronologo}
+                width={500}
+                height={500}
+                alt="Cronolog Logo"
+                />            
             <form action="">
                 {/* parte das informações do usuário */}
 
-                <div className={login.userArea}>
-                    <label htmlFor="User" className={login.label}>Usuário</label>
-                    <input type="text" placeholder='Usuário' className={login.infoArea} name="user" id="User" />
+                <div className={loginStyle.userArea}>
+                    <label htmlFor="User" className={loginStyle.label}>Usuário</label>
+                    <input type="text" placeholder='Usuário' className={loginStyle.infoArea} name="user" id="User" value={emailInput} onChange={e => setEmailInput(e.target.value)}/>
 
-                    <label htmlFor="Password" className={login.label}>Senha</label>
-                    <input type="password" placeholder='Senha' className={login.infoArea} name="password" id="Password" />
+                    <label htmlFor="Password" className={loginStyle.label}>Senha</label>
+                    <input type="password" placeholder='Senha' className={loginStyle.infoArea} name="password" id="Password" value={passwordInput} onChange={e => setPasswordInput(e.target.value)} />
                 </div>
 
                 {/* parte dos botões */}
-                <div className={login.logArea}>
-                    <input className={login.btn} type="button" value="Login" />
-                    <input className={login.btn} type="button" value="Registrar" />
+                <div className={loginStyle.logArea}>
+                    <input className={loginStyle.btn} type="button" value="Login" onClick={handleLogin} />
+                    <input className={loginStyle.btn} type="button" value="Registrar" />
                 </div>
-                <a href="/" className={login.forgetPassword}>Esqueci minha senha</a>
+                <a href="/" className={loginStyle.forgetPassword}>Esqueci minha senha</a>
             </form>
         </div>
     )
