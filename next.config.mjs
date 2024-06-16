94e0b1f5fb2e7b50925ca7924f16711d3ebf8e77
Pablo <pablorsantana@hotmail.com>
2024-06-16 15:51:19 -0300

    adding configs to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from 'next-intl/plugin';
+const withNextIntl = createNextIntlPlugin();
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    headers: async () => {
+        return[
+            {
+                source: '/api/:path*',
+                headers: [
+                    {key: 'Acess-Control-Allow-Origin', value: '*'}
+                ]
+            }
+        ]
+    }
+};
+
+export default withNextIntl(nextConfig);
