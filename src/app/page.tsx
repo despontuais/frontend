f82e28ac3462e9d6dced63c383f79e7ddc8d01c4
Pablo <pablorsantana@hotmail.com>
2024-06-17 19:03:33 -0300

    fixing frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginScreen from './../components/LoginScreen';
 import Register from './../components/Register';
+import { HomePage } from '@/components/home';
+import { NextUIProvider } from '@nextui-org/system';
 
 export default function Home() {
   return (
-    <div>
-      <Register/>
-    </div>
+    <NextUIProvider>
+      <HomePage/>
+    </NextUIProvider>
   );
 }
